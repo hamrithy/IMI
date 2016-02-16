9aa978ad7a3196ad69f18dcf6e8afc89a48ebf4a
HuySokhom <kom.huy@gmail.com>
2016-02-16 17:44:22 +0700

    add new module file for create invoice

---
+++ includes/modules/OSC/Invoice/Collection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace OSC\Invoice;
+
+use Aedea\Core\Database\StdCollection;
+
+class Collection extends StdCollection {
+	
+	public function __construct( $params = array() ){
+		parent::__construct($params);
+		
+		$this->addTable('invoice', 'i');
+		$this->idField = 'i.id';
+		$this->setDistinct(true);
+		
+		$this->objectType = __NAMESPACE__ . '\Object';		
+	}
+
+	public function filterById( $arg ){
+		if($arg){
+			$this->addWhere("i.id = '" . (int)$arg. "' ");
+		}
+	}
+
+}
---
+++ includes/modules/OSC/Invoice/Object.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace OSC\Invoice;
+
+use
+	Aedea\Core\Database\StdObject as DbObj
+	, OSC\InvoiceDetail\Collection
+		as  InvoiceDetailCol
+;
+
+class Object extends DbObj {
+		
+	protected
+		$invoiceNo
+		, $customerTypeId
+		, $doctorId
+		, $total
+		, $noted
+		, $payType
+		, $subTotal
+		, $discount
+		, $discountType
+		, $totalDiscount
+		, $grandTotal
+		, $deposit
+		, $balance
+		, $detail
+	;
+
+	public function __construct( $params = array() ){
+		parent::__construct($params);
+		$this->detail = new InvoiceDetailCol();
+	}
+
+	public function toArray( $params = array() ){
+		$args = array(
+			'include' => array(
+				'id',
+				'invoice_no',
+				'customer_type_id',
+				'doctor_id',
+				'noted',
+				'total',
+				'pay_type',
+				'sub_total',
+				'discount',
+				'discount_type',
+				'total_discount',
+				'grand_total',
+				'deposit',
+				'balance',
+				'detail'
+			)
+		);
+
+		return parent::toArray($args);
+	}
+	
+	public function load( $params = array() ){
+		$q = $this->dbQuery("
+			SELECT
+				invoice_no,
+				customer_type_id,
+				doctor_id,
+				noted,
+				total,
+				pay_type,
+				sub_total,
+				discount,
+				discount_type,
+				total_discount,
+				grand_total,
+				deposit,
+				balance
+			FROM
+				invoice
+			WHERE
+				id = '" . (int)$this->getId() . "'	
+		");
+		
+		if( ! $this->dbNumRows($q) ){
+			throw new \Exception(
+				"404: Invoice not found",
+				404
+			);
+		}
+		$this->setProperties($this->dbFetchArray($q));
+		$this->detail->setFilter('id', $this->getInvoiceNo());
+		$this->detail->populate();
+	}
+
+	public function update($id){
+		if( !$id ) {
+			throw new Exception("save method requires id to be set");
+		}
+		$this->dbQuery("
+			UPDATE
+				customer_type
+			SET
+				name = '" . $this->getName() . "',
+				description = '" . $this->getDescription() . "'
+			WHERE
+				id = '" . (int)$id . "'
+		");
+
+	}
+
+	public function delete($id){
+		if( !$id ) {
+			throw new Exception("delete method requires id to be set");
+		}
+		$this->dbQuery("
+			DELETE FROM
+				customer_type
+			WHERE
+				id = '" . (int)$id . "'
+		");
+	}
+
+	public function insert(){
+		$this->dbQuery("
+			INSERT INTO
+				invoice
+			(
+				invoice_no,
+				customer_type_id,
+				doctor_id,
+				noted,
+				total,
+				pay_type,
+				sub_total,
+				discount,
+				discount_type,
+				total_discount,
+				grand_total,
+				deposit,
+				balance,
+				create_by,
+				create_date
+			)
+				VALUES
+			(
+				'" . $this->getInvoiceNo() . "',
+				'" . $this->getCustomerTypeId() . "',
+				'" . $this->getDoctorId() . "',
+				'" . $this->getNoted() . "',
+				'" . $this->getTotal() . "',
+				'" . $this->getPayType() . "',
+				'" . $this->getSubTotal() . "',
+				'" . $this->getDiscount() . "',
+				'" . $this->getDiscountType() . "',
+				'" . $this->getTotalDiscount() . "',
+				'" . $this->getGrandTotal() . "',
+				'" . $this->getDeposit() . "',
+				'" . $this->getBalance() . "',
+				'" . $this->getCreateBy() . "',
+ 				NOW()
+			)
+		");
+		$this->setId( $this->dbInsertId() );
+	}
+
+
+	public function setDetail( $string ){
+		$this->detail = $string;
+	}
+
+	public function getDetail(){
+		return $this->detail;
+	}
+
+	public function setInvoiceNo( $string ){
+		$this->invoiceNo = $string;
+	}
+
+	public function getInvoiceNo(){
+		return $this->invoiceNo;
+	}
+
+	public function setNoted( $string ){
+		$this->noted = $string;
+	}
+	
+	public function getNoted(){
+		return $this->noted;
+	}
+
+	public function setTotal( $string ){
+		$this->total = $string;
+	}
+
+	public function getTotal(){
+		return $this->total;
+	}
+
+	public function setGrandTotal( $string ){
+		$this->grandTotal = $string;
+	}
+
+	public function getGrandTotal(){
+		return $this->grandTotal;
+	}
+
+	public function setSubTotal( $string ){
+		$this->subTotal = $string;
+	}
+
+	public function getSubTotal(){
+		return $this->subTotal;
+	}
+	public function setPayType( $string ){
+		$this->payType = $string;
+	}
+
+	public function getPayType(){
+		return $this->payType;
+	}
+	public function setDiscount( $string ){
+		$this->discount = $string;
+	}
+
+	public function getDiscount(){
+		return $this->discount;
+	}
+	public function setDiscountType( $string ){
+		$this->discountType = $string;
+	}
+
+	public function getDiscountType(){
+		return $this->discountType;
+	}
+	public function setTotalDiscount( $string ){
+		$this->totalDiscount = $string;
+	}
+
+	public function getTotalDiscount(){
+		return $this->totalDiscount;
+	}
+	public function setDeposit( $string ){
+		$this->deposit = $string;
+	}
+
+	public function getDeposit(){
+		return $this->deposit;
+	}
+	public function setBalance( $string ){
+		$this->balance = $string;
+	}
+
+	public function getBalance(){
+		return $this->balance;
+	}
+	public function setDoctorId( $string ){
+		$this->doctorId = $string;
+	}
+
+	public function getDoctorId(){
+		return $this->doctorId;
+	}
+	public function setCustomerTypeId( $string ){
+		$this->customerTypeId = $string;
+	}
+
+	public function getCustomerTypeId(){
+		return $this->customerTypeId;
+	}
+
+}
